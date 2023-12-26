c992ddfd850ddb3c8e70925d84b17d6f33064961
mnsrulz <manishrawat4u@gmail.com>
2023-12-26 16:26:08 +0000

    fix the range slider

--- app/RangeSlider.tsx
+++ app/RangeSlider.tsx
@@ -7,25 +7,23 @@ import * as Slider from '@radix-ui/react-slider';
 export function RangeSlider(props: UseRangeProps) {
   const { start, range, canRefine, refine } = useRange(props);
   const { min, max } = range;
-  const [value, setValue] = useState([min, max]);
+  const [value, setValue] = useState([min || 0, max || 0]);
 
-  const from = Math.max(min, Number.isFinite(start[0]) ? start[0] : min);
-  const to = Math.min(max, Number.isFinite(start[1]) ? start[1] : max);
+  const from = min && start[0] && Math.max(min, Number.isFinite(start[0]) ? start[0] : min);
+  const to = max && start[1] && Math.min(max, Number.isFinite(start[1]) ? start[1] : max);
 
   useEffect(() => {
-    setValue([from, to]);
+    setValue([from || 0, to || 0]);
   }, [from, to]);
 
   return (
     <Slider.Root
-    name='hello'
-      
       className="slider-root"
       min={min}
       max={max}
       value={value}
       onValueChange={setValue}
-      onValueCommit={refine}
+      onValueCommit={(v) => { refine([v[0], v[1]]) }}
       disabled={!canRefine}
     >
       <Slider.Track className="slider-track">
